f92eedfd6c3e5adaed45b4761c48dcc8f6b80ee5
TNRtn <137277969+TNRtn@users.noreply.github.com>
2024-11-15 12:02:10 +0530

    Update urls.py

--- lt/lt/urls.py
+++ lt/lt/urls.py
@@ -18,7 +18,9 @@ from django.urls import path,include
 from language import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 urlpatterns = [
+    path('', RedirectView.as_view(url='/signup/')), 
     path('admin/', admin.site.urls),
     path('tnr/',views.sample),
     path('signup/',views.signupc,name='signup'),
